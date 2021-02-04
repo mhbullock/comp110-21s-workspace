bf65784c4a7b7943f204a1a61f98b8b9e7e47a63
Makayla Bullock <mhbullock@unc.edu>
2021-02-04 17:24:14 -0500

    2/4 ex01 updates

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -5,6 +5,17 @@ __author__ = "730394070"
 
 # Begin your solution here...
 
-tar_heel: int = int(print("Enter an int: "))
+tar_heel: int = int(input("Enter an int: "))
 
-if tar_heel
+
+if tar_heel % 2 == 0 and tar_heel % 7 == 0:
+    print("TAR HEELS")
+else: 
+    if tar_heel % 2 == 0: 
+        print("TAR")
+    else:
+        if tar_heel % 7 == 0:
+            print("HEELS")
+        else:
+            if tar_heel % 2 != 0 and tar_heel % 7 != 0: 
+                print("CAROLINA")
